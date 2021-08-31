440a5caa7a09c5fa85e9f4a2d0cd32ec8ac565cc
arthurs14 <arthurcs426@gmail.com>
2021-08-31 16:00:30 -0700

    adding more basics

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,54 @@
 "use strict";
+// Basic Types
 let id = 5;
-console.log(`ID: ${id}`);
+let company = 'YG Entertainment';
+let isTrainee = true;
+let x = 'Hello';
+x = 55;
+// Arrays
+let ids = [1, 2, 3, 4, 5];
+let arr = [1, true, 'random'];
+// Tuple
+let person = [1, 'Jisoo', true];
+// Tuple Array
+let employee;
+employee = [
+    [1, 'Jennie'],
+    [2, 'Rose'],
+    [3, 'Lisa'],
+];
+// Union (hold more than one type)
+let pid;
+pid = 22;
+pid = '22';
+// Enum
+var Direction1;
+(function (Direction1) {
+    Direction1[Direction1["Up"] = 1] = "Up";
+    Direction1[Direction1["Down"] = 2] = "Down";
+    Direction1[Direction1["Left"] = 3] = "Left";
+    Direction1[Direction1["Right"] = 4] = "Right";
+})(Direction1 || (Direction1 = {}));
+;
+console.log('Enum Direction1:');
+console.log(Direction1.Up);
+console.log(Direction1.Down);
+console.log(Direction1.Left);
+console.log(Direction1.Right);
+var Direction2;
+(function (Direction2) {
+    Direction2["Up"] = "Up";
+    Direction2["Down"] = "Down";
+    Direction2["Left"] = "Left";
+    Direction2["Right"] = "Right";
+})(Direction2 || (Direction2 = {}));
+;
+console.log('Enum Direction2:');
+console.log(Direction2.Up);
+console.log(Direction2.Down);
+console.log(Direction2.Left);
+console.log(Direction2.Right);
+const user = {
+    id: 1,
+    name: 'John',
+};
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,65 @@
+// Basic Types
 let id: number = 5;
+let company: string = 'YG Entertainment';
+let isTrainee: boolean = true;
+let x: any = 'Hello';
+x = 55;
 
-console.log(`ID: ${id}`);
+// Arrays
+let ids: number[] = [1,2,3,4,5];
+let arr: any[] = [1, true, 'random'];
+
+// Tuple
+let person: [number, string, boolean] = [1, 'Jisoo', true];
+
+// Tuple Array
+let employee: [number, string][];
+
+employee = [
+  [1, 'Jennie'],
+  [2, 'Rose'],
+  [3, 'Lisa'],
+];
+
+// Union (hold more than one type)
+let pid: string | number;
+pid = 22;
+pid = '22';
+
+// Enum
+enum Direction1 {
+  Up = 1,
+  Down,
+  Left,
+  Right
+};
+
+console.log('Enum Direction1:')
+console.log(Direction1.Up);
+console.log(Direction1.Down);
+console.log(Direction1.Left);
+console.log(Direction1.Right);
+
+enum Direction2 {
+  Up = 'Up',
+  Down = 'Down',
+  Left = 'Left',
+  Right = 'Right'
+};
+
+console.log('Enum Direction2:')
+console.log(Direction2.Up);
+console.log(Direction2.Down);
+console.log(Direction2.Left);
+console.log(Direction2.Right);
+
+// Objects
+type User = {
+  id: number,
+  name: string,
+} 
+
+const user: User = {
+  id: 1,
+  name: 'John',
+};
